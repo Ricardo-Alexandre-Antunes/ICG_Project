bcb8f33542112a77793b4e78b3c14f1f61461445
Ricardo-Alexandre-Antunes <ricardo.alexandre.antunes@ua.pt>
2025-03-19 18:38:37 +0000

    updated diary

--- src/scene/test_main.js
+++ src/scene/test_main.js
@@ -1,6 +1,3 @@
-import * as THREE from 'three';
 import GameWorld from '../scene/GameWorld.js';
-import Character_Ski from '../models/Character.js';
-import Mountain from '../models/Mountain.js';
 
 new GameWorld();
